4cf56b6def13b8401b65b3e03c2293e2f4cbd917
lisah <Saturn95>
2021-01-09 19:00:05 +0100

    add controller sample

---
+++ src/fhtw/Controller.java
@@ -0,0 +1,40 @@
+package fhtw;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+
+public class Controller {
+
+    @FXML
+    private TitledPane login_window;
+
+    @FXML
+    private AnchorPane inner_login;
+
+    @FXML
+    private PasswordField psw_btn;
+
+    @FXML
+    private PasswordField user_btn;
+
+    @FXML
+    private Label lbl_usr;
+
+    @FXML
+    private Label lbl_pwd;
+
+    @FXML
+    private Label welcome_txt;
+
+    @FXML
+    private Button lgn_btn;
+
+}
+
