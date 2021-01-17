7de60bf27ce8664a2344a165693f2a17ff484db6
Nicole Hammerer <ic19b094@technikum-wien.at>
2021-01-17 19:44:52 +0100

    simple singleplayer game logic added

--- src/fhtw/Gameplay.java
+++ src/fhtw/Gameplay.java
@@ -2,42 +2,46 @@ package fhtw;
 
 import com.google.gson.*;
 
+import javax.xml.transform.Result;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Gameplay {
 
     static void singleplay(JsonObject json) {
 
-        Gson gson = new Gson();
+        Integer user_points = 0;
 
-        String json_string = json.toString();
-        //Results[] resultArray = gson.fromJson(json,  Results[].class);
+        //create Array of questions
+        JsonArray questionArray = (JsonArray) json.get("results");
 
-        /*
-        for(Results result : resultArray) {
-            System.out.println(result.getResults());
-        }
-        */
-
-
-      /*  JsonArray namearr = (JsonArray) json.get("results");
-
-        Gson gson = new GsonBuilder().setPrettyPrinting().create(); // pretty print
-
-        for (Object objInArr : namearr) {
-            JsonObject jsonFrage = (JsonObject) objInArr;
+        //for each question:
+        for (JsonElement question : questionArray){
+            //create Object for question
+            JsonObject jsonQuestion = (JsonObject) question;
+            //create list to store all possible answers
             List<JsonElement> answers = new ArrayList<>();
-            answers.add(jsonFrage.get("correct_answer"));
-            answers.add(jsonFrage.get("incorrect_answers"));
+            //first store the correct answer
+            JsonElement correct_answer = jsonQuestion.get("correct_answer");
+            answers.add(correct_answer);
+            //create a new Array for the nested values of the incorrect answers
+            JsonArray incorrect_answer = (JsonArray) jsonQuestion.get("incorrect_answers");
+            //for each from the incorrect answers add them to our answers-Array
+            for (JsonElement answer : incorrect_answer){
+                answers.add(answer);
+            }
+            System.out.println(jsonQuestion.get("question"));
+            System.out.println(answers);
+
+            //player answers
+
+            String round_stats = get_answer(correct_answer, user_points);
+            System.out.println(round_stats);
+            System.out.println("The correct answer was: " + correct_answer + "\n");
+            //
         }
-
-*/
-
-/*
-        String [] answer_list = new String[]{results.getCorrect_answer(), incorrect_results.getIncorrect_1(), incorrect_results.getIncorrect_2(), incorrect_results.getIncorrect_3()};
-        System.out.println(answer_list[create_random_question_order.shuffle()]);
-        System.out.println(Arrays.toString(answer_list));
-*/
     }
 
 
@@ -49,5 +53,22 @@ public class Gameplay {
             return int_random;
         }
 
+    static String get_answer(JsonElement correct_answer, Integer user_points) {
+
+        Scanner user = new Scanner(System.in);
+        String user_answer = user.nextLine();
+        String message = null;
+
+        //TODO Punktezahl (user points) speichern und übernehmen
+        if (!user_answer.equals(correct_answer.toString())) {
+            message = "Oh no, " + user_answer + " is the wrong answer!";
+        } else {
+            //idee: array mit messages erstellen von denen random eine geprinted wird
+            message = "Correct answer! Points: ";
+            user_points++;
+        }
+
+        return message ;
+    }
 
 }
