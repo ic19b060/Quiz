060f56b0cc50034a37d4ebb1f3bdf8eef582958b
lisah <Saturn95>
2021-01-28 15:10:20 +0100

    server_client

--- src/fhtw/Client.java
+++ src/fhtw/Client.java
@@ -5,17 +5,37 @@ import javafx.stage.Stage;
 import java.io.*;
 import java.net.*;
 
-public class Client extends Application {
+public class Client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        DataOutputStream out;
+        Socket client = null;
+        try {
+            client = new Socket("localhost", 1111);
+            System.out.println("Client connected to " + client.getInetAddress());
 
+
+            System.out.println("Just connected to " + client.getRemoteSocketAddress());
+            OutputStream outToServer = client.getOutputStream();
+            out = new DataOutputStream(outToServer);
+
+            out.writeUTF("Hello from " + client.getLocalSocketAddress());
+            out.flush();
+
+            InputStream inFromServer = client.getInputStream();
+            DataInputStream in = new DataInputStream(inFromServer);
+
+            System.out.println("Server says " + in.readUTF());
+
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-
-    }
 }
 
 
--- src/fhtw/Server.java
+++ src/fhtw/Server.java
@@ -1,33 +1,92 @@
-
 package fhtw;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.*;
 import java.net.*;
 
-public class Server {
+public class Server extends Application {
 
-    public static void main(String[] args) {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-        try{
-            ServerSocket server = new ServerSocket(1111, 10);
-            System.out.println("Server waiting for connection");
-            Socket client = server.accept(); //warten bis Verbindung hergestellt wurde
+        Parent root = FXMLLoader.load(getClass().getResource("loginWindow.fxml"));
 
-            //while (true) {
-                System.out.println("Server connected to " + client.getInetAddress());
-                byte[] b = new byte[100]; //array mit 100 Stellen
-                InputStream in = client.getInputStream();
+        primaryStage.setTitle("Login_Quiz");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(1111, 10);
+
+        while (true) {
+            try {
+
+                System.out.println("Server waiting for connection");
+                Socket client = server.accept(); //warten bis Verbindung hergestellt wurde
 
 
-                //OutputStream out = client.getOutputStream();
+                System.out.println("Just connected to " + client.getInetAddress());
+                DataInputStream in = new DataInputStream(client.getInputStream());
 
-                //messages werden in bytes verschickt
-                int bytes = in.read(b); //Rückgabewert = gelesene Bytes (int)
-                System.out.println("received " + bytes);
-                String s = new String(b);
-                System.out.println("message: " + s);
+                System.out.println(in.readUTF());
+                DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress());
+                out.flush();
 
-            //}
-        } catch (Exception e){
+                ClientHandler clientSock
+                        = new ClientHandler(client,in,out);
+
+                // This thread will handle the client
+                // separately
+                new Thread(clientSock).start();
+
+
+                //}  //while (true) {
+                //            System.out.println("Server connected to " + client.getInetAddress());
+                //            byte[] b = new byte[100]; //array mit 100 Stellen
+                //            InputStream in = client.getInputStream();
+                //
+                //
+                //            //OutputStream out = client.getOutputStream();
+                //
+                //            //messages werden in bytes verschickt
+                //            int bytes = in.read(b); //Rückgabewert = gelesene Bytes (int)
+                //            System.out.println("received " + bytes);
+                //            String s = new String(b);
+                //            System.out.println("message: " + s);
+                launch(args);
+            } catch (Exception e) {
+                server.close();
+                e.printStackTrace();
+            }
+
         }
+
+
+    }
+
+    static class ClientHandler extends Thread {
+
+        final Socket server;
+        final DataInputStream in;
+        final DataOutputStream out;
+
+        public ClientHandler(Socket server, DataInputStream in, DataOutputStream out) {
+            this.server = server;
+            this.in = in;
+            this.out = out;
+        }
+
+
+        public void run() {
+
+        }
+
     }
 }
